cb3d588d089e99741c65678c1bcf1c86b8a9ec32
volkanmetin <ben@volkanmetin.com>
2024-12-06 16:56:32 +0200

    ++

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,5 +4,5 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\FortifyServiceProvider::class,
     App\Providers\HorizonServiceProvider::class,
-    App\Providers\TelescopeServiceProvider::class,
+    //App\Providers\TelescopeServiceProvider::class,
 ];
